6e53a9054d466d7fdd694ad04ccf51508b056cc9
alex kelly <kelster101757@hotmail.com>
2023-10-30 21:47:28 +0000

    further updates lsuv blog

--- copy_lsuv_from_colab.bat
+++ copy_lsuv_from_colab.bat
@@ -1,3 +1,3 @@
 @echo off
-copy "G:\My Drive\Colab Notebooks\LSUV_for_the_blog.ipynb" "C:\development\github projects\AlexPaulKelly\posts\demo_quarto_jupyter_ref"
-quarto preview "C:\development\github projects\AlexPaulKelly\posts\demo_quarto_jupyter_ref\LSUV_for_the_blog.ipynb"
+copy "G:\My Drive\Colab Notebooks\LSUV_for_the_blog.ipynb" ".\posts\demo_quarto_jupyter_ref"
+quarto preview ".\posts\demo_quarto_jupyter_ref\LSUV_for_the_blog.ipynb"
