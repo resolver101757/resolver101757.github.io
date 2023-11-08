693a1902ad78f800fbfe3517740395d5a0ecfd82
alex kelly <kelster101757@hotmail.com>
2023-11-08 21:54:29 +0000

    published lsuv blog

--- copy_lsuv_from_colab.bat
+++ copy_lsuv_from_colab.bat
@@ -1,3 +1,3 @@
 @echo off
-copy "G:\My Drive\Colab Notebooks\LSUV_for_the_blog.ipynb" ".\posts\demo_quarto_jupyter_ref"
-quarto preview ".\posts\demo_quarto_jupyter_ref\LSUV_for_the_blog.ipynb"
+copy "G:\My Drive\Colab Notebooks\LSUV_for_the_blog.ipynb" ".\posts\initialization_neural_networks\index.ipynb"
+quarto preview ".\posts\initialization_neural_networks\index.ipynb"
